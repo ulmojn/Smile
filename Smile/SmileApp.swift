33bab34e38f3f64e9627a704fb152701a5875114
Ulrik Lind <ulriklind@gmail.com>
2023-08-01 14:08:50 +0200

    Initial Commit

---
+++ Smile/SmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileApp.swift
+//  Smile
+//
+//  Created by U. Lind Mortensen on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
